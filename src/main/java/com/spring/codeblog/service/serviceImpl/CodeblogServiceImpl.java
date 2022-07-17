2a0e80f8c978a52561c99ac887516479a0e9f778
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2022-07-17 09:25:12 -0300

    Aula 3 - Populando tabela

--- src/main/java/com/spring/codeblog/service/serviceImpl/CodeblogServiceImpl.java
+++ src/main/java/com/spring/codeblog/service/serviceImpl/CodeblogServiceImpl.java
@@ -1,14 +1,16 @@
 package com.spring.codeblog.service.serviceImpl;
 import com.spring.codeblog.model.Post;
-import com.spring.codeblog.repository.CodeBlogRepository;
+import com.spring.codeblog.repository.CodeblogRepository;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CodeblogServiceImpl implements CodeblogService {
     @Autowired
-    CodeBlogRepository codeBlogRepository;
+    CodeblogRepository codeBlogRepository;
 
     @Override
     public List<Post> findAll() {

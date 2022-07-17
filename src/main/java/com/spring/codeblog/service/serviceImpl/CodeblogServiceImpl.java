cb04bcb657be4b960089c71861e242eabb920cca
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2022-07-16 22:19:07 -0300

    Aula 3 - Métodos de serviceImpl

--- src/main/java/com/spring/codeblog/service/serviceImpl/CodeblogServiceImpl.java
+++ src/main/java/com/spring/codeblog/service/serviceImpl/CodeblogServiceImpl.java
@@ -1,21 +1,27 @@
 package com.spring.codeblog.service.serviceImpl;
 import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeBlogRepository;
 import com.spring.codeblog.service.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 
 public class CodeblogServiceImpl implements CodeblogService {
+    @Autowired
+    CodeBlogRepository codeBlogRepository;
+
     @Override
     public List<Post> findAll() {
-        return null;
+        return codeBlogRepository.findAll();
     }
 
     @Override
     public Post findById(long id) {
-        return null;
+        return codeBlogRepository.findById(id).get();
     }
 
     @Override
     public Post save(Post post) {
-        return null;
+        return codeBlogRepository.save(post);
     }
 }

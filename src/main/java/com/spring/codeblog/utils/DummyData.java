bec58a8edb4acf22b6336f215f0c9455b5d0dbd1
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2022-07-18 18:19:50 -0300

    Aula 3 finalizada

--- src/main/java/com/spring/codeblog/utils/DummyData.java
+++ src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -16,12 +16,12 @@ public class DummyData {
     @Autowired
     CodeblogRepository codeblogRepository;
 
-    @PostConstruct
+    //@PostConstruct
     public void savePosts(){
 
         List<Post> postList = new ArrayList<>();
         Post post1 = new Post();
-        post1.setAutor("Bruno Alexandre");
+        post1.setAutor("Michelli Brito");
         post1.setData(LocalDate.now());
         post1.setTitulo("Docker");
         post1.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
@@ -36,7 +36,7 @@ public class DummyData {
         postList.add(post2);
 
         for(Post post: postList){
-            Post postSaved = blogRepository.save(post);
+            Post postSaved = codeblogRepository.save(post);
             System.out.println(postSaved.getId());
         }
 

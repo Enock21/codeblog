c15054e37e6ae22b06c0be95ab0b0e6805dee264
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2022-07-28 18:27:41 -0300

    Aula 7 - Bug encontrado
    Ao finalizar o método getPostDetails, tentei testar a funcionalidade ao acessar, na url da aplicação, http://localhost:8080/posts/3, sendo 3 o id de um dos posts adicionados. Uma página de erro foi retornada

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,14 @@ public class CodeblogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts", posts); //Indica que quando se acessa o atributo "posts", se tem acesso a uma listagem com todos os posts no BD
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/[id]", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post); //Indica que o objeto post será passado como model e renderizado na view
         return mv;
     }
 }
